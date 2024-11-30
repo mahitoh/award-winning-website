b60f50c948e013bbaeee372bfc60d3fa3546c8dc
Strangemorgan <ankiambomrichcal06@gmail.com>
2024-11-30 18:04:04 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
+import Navbar from "./components/Navbar.jsx";
+
+const App = () => {
+    return (
+        <main className="relative min-h-screen w-screen overflow-x-hidden">
+            <Navbar />
+         <Hero />
+            <About />
+        </main>
+    );
+};
+
+export default App;
